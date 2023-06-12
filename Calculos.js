2832f4fc19851af053a55ca9547b74085f15dcb0
Oswaldo Cruz <kikke8508@gmail.com>
2023-06-12 15:35:13 -0600

    Validaciones en inputs

--- Calculos.js
+++ Calculos.js
@@ -1,78 +1,84 @@
-function PolarizacionFija(){
+var $select1 = document.getElementById('Opc1');
+$select1.addEventListener("change", opcionCambiada);
 
-var Vbe = 0.7;
+var $select2 = document.getElementById('Opc2');
+$select2.addEventListener("change", opcionCambiada2);
+var B = 0;
 
-var B = 80;
+function opcionCambiada() {
+    var calculo = $select1.selectedIndex;
+    var IB = document.getElementById('PF-IB');
+    var IC = document.getElementById('PF-IC');
+    var VC = document.getElementById('PF-VC');
+    var VB = document.getElementById('PF-VB');
+    var VCE = document.getElementById('PF-VCE');
+    var VCC = document.getElementById('PF-VCC');
+    var RC = document.getElementById('PF-RB');
+    var RB = document.getElementById('PF-RC');
 
-var Vcc = Number(document.getElementById('PF-VCC').value);
+    if (calculo == 1) {
+        IB.disabled = true;
+        IC.disabled = true;
+        VC.disabled = true;
+        VB.disabled = true;
+        VCE.disabled = true;
+        VCC.disabled = false;
+        RC.disabled = false;
+        RB.disabled = false;
+    } else {
+        VCC.disabled = true;
+        RC.disabled = true;
+        RB.disabled = true;
+        IB.disabled = false;
+        IC.disabled = false;
+        VC.disabled = false;
+        VB.disabled = false;
+        VCE.disabled = false;
+    }
+};
 
-var Rb = Number(document.getElementById('PF-RB').value);
+function opcionCambiada2() {
+    var transistor = $select2.selectedIndex;
+    if (transistor == 1) {
+        B = 80;
+        console.log(B);
+    } else if (transistor == 2) {
+        B = 90;
+        console.log(B);
+    } else {
+        B = 100;
+        console.log(B);
+    }
+}
 
-var Rc = Number(document.getElementById('PF-RC').value);
+function PolarizacionFija() {
 
+    var Vbe = 0.7;
+    var Vcc = Number(document.getElementById('PF-VCC').value);
+    var Rb = Number(document.getElementById('PF-RB').value);
+    var Rc = Number(document.getElementById('PF-RC').value);
 
-// Obtener IB
+    // Obtener IB
+    var Ib = (Vcc - Vbe) / Rb;
+    document.getElementById('PF-IB').value = Ib
 
+    // Obtener IC
+    var Ic = Ib * B;
+    document.getElementById('PF-IC').value = Ic
 
-var Ib = (Vcc - Vbe) / Rb;
+    // Obtener Vrc
+    var Vrc = Ic * Rc;
 
-document.getElementById('PF-IB').value = Ib
+    // Obtener VCE
+    var Vce = Vcc - Vrc;
+    document.getElementById('PF-VCE').value = Vce
 
+    // Obtener VC
+    var Vc = Vcc - Vrc;
+    document.getElementById('PF-VC').value = Vc;
 
-// Obtener IB
-
-
-// Obtener IC
-
-
-var Ic = Ib * B;
-
-document.getElementById('PF-IC').value = Ic
-
-
-// Obtener IC
-
-var Vrc = Ic * Rc;
-
-
-console.log(Ic)
-console.log(Rc)
-console.log(Vrc)
-console.log(Vcc)
-
-
-// Obtener VCE
-
-
-var Vce = Vcc - Vrc;
-
-document.getElementById('PF-VCE').value = Vce
-
-
-// Obtener VCE
-
-
-// Obtener VC
-
-
-var Vc = Vcc - Vrc;
-
-document.getElementById('PF-VC').value = Vc;
-
-
-// Obtener VC
-
-// Obtener VB
-
-
-document.getElementById('PF-VB').value = Vbe;
-
-
-// Obtener VB
-
-
-
-
+    // Obtener VB
+    document.getElementById('PF-VB').value = Vbe;
 }
 
 
