d64ba7339688b5f9a54d54188b633b9182e6c52d
Oswaldo Cruz <kikke8508@gmail.com>
2023-06-13 16:26:24 -0600

    Polarización de emisor final e Index

--- Calculos.js
+++ Calculos.js
@@ -1,8 +1,19 @@
 var $select1 = document.getElementById('Opc1');
+console.log($select1)
 $select1.addEventListener("change", opcionCambiada);
 
 var $select2 = document.getElementById('Opc2');
+console.log($select2)
 $select2.addEventListener("change", opcionCambiada2);
+
+var $select3 = document.getElementById('Opc3');
+console.log($select3)
+$select3.addEventListener("change", opcionCambiada3);
+
+var $select4 = document.getElementById('Opc4');
+console.log($select4)
+$select4.addEventListener("change", opcionCambiada4);
+
 var B = 0;
 
 function opcionCambiada() {
@@ -51,8 +62,68 @@ function opcionCambiada2() {
     }
 }
 
-function PolarizacionFija() {
+// Apartado de Polarizacion de Emisor
 
+function opcionCambiada3() {
+    var calculo = $select3.selectedIndex;
+    var IB = document.getElementById('PE-IB');
+    var IC = document.getElementById('PE-IC');
+    var IE = document.getElementById('PE-IE');
+    var VC = document.getElementById('PE-VC');
+    var VE = document.getElementById('PE-VE');
+    var VB = document.getElementById('PE-VB');
+    var VCE = document.getElementById('PE-VCE');
+    var VCC = document.getElementById('PE-VCC');
+    var RC = document.getElementById('PE-RB');
+    var RB = document.getElementById('PE-RC');
+    var RE = document.getElementById('PE-RE');
+
+
+    if (calculo == 1) {
+        IB.disabled = true;
+        IC.disabled = true;
+        IE.disabled = true;
+        VC.disabled = true;
+        VE.disabled = true;
+        VB.disabled = true;
+        VCE.disabled = true;
+        VCC.disabled = false;
+        RC.disabled = false;
+        RB.disabled = false;
+        RE.disabled = false;
+    } else {
+        IB.disabled = false;
+        IC.disabled = false;
+        IE.disabled = false;
+        VC.disabled = false;
+        VE.disabled = false;
+        VB.disabled = false;
+        VCE.disabled = false;
+        VCC.disabled = false;
+        RC.disabled = true;
+        RB.disabled = true;
+        RE.disabled = true;
+    }
+};
+
+function opcionCambiada4() {
+    var transistor = $select4.selectedIndex;
+    if (transistor == 1) {
+        B = 80;
+        console.log(B);
+    } else if (transistor == 2) {
+        B = 90;
+        console.log(B);
+    } else {
+        B = 100;
+        console.log(B);
+    }
+}
+
+// Fin apartado de Polarizacion de Emisor
+
+
+function PolarizacionFija() {
     var Vbe = 0.7;
     var Vcc = Number(document.getElementById('PF-VCC').value);
     var Rb = Number(document.getElementById('PF-RB').value);
@@ -81,7 +152,76 @@ function PolarizacionFija() {
     document.getElementById('PF-VB').value = Vbe;
 }
 
-function RealimentacionColector(){
+function PolarizacionEmisor() {
+    var calculo = $select3.selectedIndex;
+    if (calculo == 1) {
+        console.log("Corrientes y voltajes!");
+        var Vbe = 0.7;
+        var Vcc = Number(document.getElementById('PE-VCC').value);
+        var Rb = Number(document.getElementById('PE-RB').value);
+        var Rc = Number(document.getElementById('PE-RC').value);
+        var Re = Number(document.getElementById('PE-RE').value);
+
+        // Obtener IB
+        var Ib = (Vcc - Vbe) / (Rb + (B + 1) * Re);
+        document.getElementById('PE-IB').value = Ib
+
+        // Obtener IC
+        var Ic = Ib * B;
+        document.getElementById('PE-IC').value = Ic;
+
+        // Obtener IE
+        var Ie = Ib + Ic;
+        document.getElementById('PE-IE').value = Ie;
+
+        // Obtener Vrc
+        var Vrc = Ic * Rc;
+
+        // Obtener VE
+        var Ve = Re * Ie;
+        document.getElementById('PE-VE').value = Ve;
+
+        // Obtener VCE
+        var VCE = Vcc - Vrc - Ve;
+        document.getElementById('PE-VCE').value = VCE;
+
+        //Obtener Vc
+        var Vc = VCE + Ve;
+        document.getElementById('PE-VC').value = Vc;
+
+        console.log(Vbe);
+        console.log(Ve);
+        //Obtener Vb
+        var Vb = Vbe + Ve;
+        document.getElementById('PE-VB').value = Vb;
+    } else {
+        console.log("Resistencias!");
+        var Vbe = 0.7;
+        var Vcc = Number(document.getElementById('PE-VCC').value);
+        var Ib = Number(document.getElementById('PE-IB').value);
+        var Ic = Number(document.getElementById('PE-IC').value);
+        var Ie = Number(document.getElementById('PE-IE').value);
+        var Vc = Number(document.getElementById('PE-VC').value);
+        var Vb = Number(document.getElementById('PE-VB').value);
+        var Ve = Number(document.getElementById('PE-VE').value);
+        var Vce = Number(document.getElementById('PE-VCE').value);
+
+        // Obtener RE
+        var Re = Ve / Ie;
+        document.getElementById('PE-RE').value = Re;
+
+        // Obtener RC
+        var Vrc = Vcc - Ve - Vce;
+        var Rc = Vrc / Ic;
+        document.getElementById('PE-RC').value = Rc;
+
+        //Obtener RB
+        var Rb = (Vcc - Vbe - (Ib * (B + 1) * Re)) / Ib;
+        document.getElementById('PE-RB').value = Rb;
+    }
+}
+
+function RealimentacionColector() {
 
     var B = 80;
 
@@ -90,17 +230,17 @@ function RealimentacionColector(){
     var Rb = Number(document.getElementById('PRC-RB').value);
     var Rc = Number(document.getElementById('PRC-RC').value);
     var Re = Number(document.getElementById('PRC-RE').value);
-    
-     // Obtener IB
-     var Ib = (Vcc - Vbe) / (Rb + B * (Rc + Re));
-     document.getElementById('PRC-IB').value = Ib
 
-     // Obtener IC
-     var Ic = Ib * B;
-     document.getElementById('PRC-IC').value = Ic
+    // Obtener IB
+    var Ib = (Vcc - Vbe) / (Rb + B * (Rc + Re));
+    document.getElementById('PRC-IB').value = Ib
+
+    // Obtener IC
+    var Ic = Ib * B;
+    document.getElementById('PRC-IC').value = Ic
 
     // Obtener VCE
-    var Vce = Vcc - Ic*(Rc+Re);
+    var Vce = Vcc - Ic * (Rc + Re);
     document.getElementById('PRC-VCE').value = Vce
 
     // Obtener Vrc
@@ -114,9 +254,7 @@ function RealimentacionColector(){
 
 }
 
-
-
-function PolarizacionDiv(){
+function PolarizacionDiv() {
     var Vbe = 0.7;
     var Re = Number(document.getElementById('PDV-RE').value);
     var Vcc = Number(document.getElementById('PDV-VCC').value);
@@ -124,15 +262,15 @@ function PolarizacionDiv(){
     var R1 = Number(document.getElementById('PDV-R1').value);
     var R2 = Number(document.getElementById('PDV-R2').value);
 
-    var Vth = (Vcc * R2)/(R1 + R2);
-    var Rth = (R1 * R2)/(R1 + R2);
-    
-    
+    var Vth = (Vcc * R2) / (R1 + R2);
+    var Rth = (R1 * R2) / (R1 + R2);
+
+
 
     //Obtener Ib
-    var Ib = (Vth - Vbe)/(Rth + (80+1)*Re);
+    var Ib = (Vth - Vbe) / (Rth + (80 + 1) * Re);
     document.getElementById('PDV-IB').value = Ib;
-    
+
     //obteber Ic
     var Ic = 80 * Ib;
     document.getElementById('PDV-IC').value = Ic;
@@ -142,7 +280,7 @@ function PolarizacionDiv(){
     var Vrc = Ic * Rc;
 
     //Obtener Vce
-    var Vce = Vcc-Vrc-Vre;
+    var Vce = Vcc - Vrc - Vre;
     document.getElementById('PDV-VCE').value = Vce;
 
     //Obtener Ve
@@ -155,5 +293,5 @@ function PolarizacionDiv(){
     //Obtener Vb
     var Vb = Vbe + Vre;
     document.getElementById('PDV-VB').value = Vb;
-    
+
 }
